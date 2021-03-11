53d32454161925113645c349ba8020acb421d369
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-11 16:46:57 +0530

    added loading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Navbar from "./Navbar";
 import CartContainer from "./CartContainer";
 
 export default function App() {
+  const { loading } = useGlobalContext();
+  
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+  
   return (
     <main>
       <Navbar />
