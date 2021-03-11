972ef44c112f8761f6fe918388499396f8bc2574
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-11 16:48:37 +0530

    cart total

--- src/CartContainer.js
+++ src/CartContainer.js
@@ -2,7 +2,7 @@ import { useGlobalContext } from "./context";
 import CartItem from "./CartItem";
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext();
+  const { cart, total } = useGlobalContext();
   if (cart.length === 0) {
     return (
       <section className="cart">
@@ -27,7 +27,7 @@ const CartContainer = () => {
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>$0.00</span>
+            total <span>${total}</span>
           </h4>
         </div>
         <button
