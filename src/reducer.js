a4402505f73cfda8e66e18f37872d8b14283dcc6
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-11 17:06:30 +0530

    added remove item -2

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,16 @@
 const reducer = (state, action) => {
-  if (action.type==='CLEAR_CART') {
-    return { ...state, cart: []}
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
   }
-}
+
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload)
+    };
+  }
+  
+  return state;
+};
 
 export default reducer;
